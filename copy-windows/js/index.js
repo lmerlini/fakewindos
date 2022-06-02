5be03d0c0ab694ee1a074ce7871b3ae9a2da2e63
Luis Merlini <merliniluis@gmail.com>
2022-06-02 09:24:42 -0300

    algumas alterações

--- copy-windows/js/index.js
+++ copy-windows/js/index.js
@@ -6,3 +6,36 @@ menu.addEventListener('click', (e) => {
     activeMenu.classList.toggle('dados-off')
     menu.classList.toggle('checked')
 })
+
+// horascopy-windows/js/index.js
+function showTimer() {
+    'use strict'; // entendi a utilização do use strict
+    let myDiv = document.getElementById("history"),
+        date = new Date(),
+        year = date.getUTCFullYear(), // aqui eu poderia ter utilizado  *** destruction js es6 ? ***
+        month = date.getUTCMonth(), // aqui eu poderia ter utilizado  *** destruction js es6 ? ***
+        day = date.getUTCDate(); // aqui eu poderia ter utilizado  *** destruction js es6 ? ***
+
+    // aqui eu poderia ter utilizado ternário? 
+    if (month < 10) {
+        month = '0' + month;
+    }
+    if (day < 10) {
+        day = '0' + day;
+    }
+    myDiv.textContent = day + "/" + month + "/" + year;
+    // aqui eu poderia ter passado para innerHTML, mas não passei acho que o content ficou melhor. *** PESQUISAR AS DIFERENÇAS NO DOM ***
+}
+function myTimer() {
+    let date = new Date();
+    document.getElementById("time").textContent = date.toLocaleTimeString(); // *** ver com buscar somente hora e minuto ***
+}
+showTimer();
+myTimer()
+
+
+
+
+// abertura de progamas.. aqui eu pesquisei na internet se era possível fazer isso
+
+
--- copy-windows/js/teste.js
+++
@@ -1,8 +0,0 @@
-const clickMenu = document.querySelector('[data-toggle="toggleMenu"]')
-
-clickMenu.addEventListener('click', (e) => {
-    let menu = document.createElement('div');
-
-
-})
-console.log(clickMenu);
